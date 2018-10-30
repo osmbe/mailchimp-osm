3dfa4ffc89b141a31c092dea3d184a3161e69bdf
Jonathan Beliën <dev@jbelien.be>
2018-10-30 19:06:14 +0100

    Update src/App/Handler/SubscribeHandler.php

--- src/App/Handler/SubscribeHandler.php
+++ src/App/Handler/SubscribeHandler.php
@@ -41,30 +41,39 @@ class SubscribeHandler implements RequestHandlerInterface
 
         $key = $request->getAttribute('list');
 
-        $file = 'data/cache/lists.json';
-        if (!file_exists($file) || !is_readable($file)) {
-            throw new Exception(sprintf('Unable to read file "%s" !', $file));
+        try {
+            $file = 'data/cache/lists.json';
+            if (!file_exists($file) || !is_readable($file)) {
+                throw new Exception(sprintf('Unable to read file "%s" !', $file));
+            }
+
+            $fileContent = file_get_contents($file);
+            if ($fileContent === false) {
+                throw new Exception(sprintf('Invalid JSON in file "%s" !', $file));
+            }
+
+            $lists = json_decode($fileContent);
+            $list = current(array_filter($lists, function ($value) use ($key) {
+                return $value->identifier === $key;
+            }));
+
+            if ($list === false) {
+                throw new Exception(sprintf('The list "%s" does not exist !', $key));
+            }
+
+            $data = [
+                'iframe' => $iframe,
+                'list'   => $list,
+            ];
+
+            return new HtmlResponse($this->template->render('app::subscribe', $data));
+        } catch (Exception $e) {
+            $data = [
+                'iframe'  => $iframe,
+                'message' => $e->getMessage(),
+            ];
+
+            return new HtmlResponse($this->template->render('error::app-error', $data));
         }
-
-        $fileContent = file_get_contents($file);
-        if ($fileContent === false) {
-            throw new Exception(sprintf('Invalid JSON in file "%s" !', $file));
-        }
-
-        $lists = json_decode($fileContent);
-        $list = current(array_filter($lists, function ($value) use ($key) {
-            return $value->identifier === $key;
-        }));
-
-        if ($list === false) {
-            throw new Exception(sprintf('The list "%s" does not exist !', $key));
-        }
-
-        $data = [
-            'iframe' => $iframe,
-            'list'   => $list,
-        ];
-
-        return new HtmlResponse($this->template->render('app::subscribe', $data));
     }
 }
